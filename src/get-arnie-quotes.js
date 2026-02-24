9bde4a844d16458fa9fb10792a47cdd5f569308b
Alan Han <alanhan@Alans-MacBook-Pro.local>
2026-02-25 08:41:52 +1100

    feat: flesh out getArnieQuotes method

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,25 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = await Promise.all(
+    urls.map(async (url) => {
+      try {
+        const response = await httpGet(url);
+
+        if (response.status === 200) {
+          const { message } = JSON.parse(response.body);
+          return { 'Arnie Quote': message };
+        } else {
+          const { message } = JSON.parse(response.body);
+          return { 'FAILURE': message };
+        }
+      } catch (error) {
+        return { 'FAILURE': error.message };
+      }
+    })
+  );
+
+  return results;
 };
 
 module.exports = {
